925295a41f2caad49e7d61f762528d02357bcb1f
Chernihaa <Chernihaa@mail.ru>
2025-02-05 07:56:19 +0900

    Добавьте файлы проекта.

---
+++ 5.5_Рекурсивные функции/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5._5_Рекурсивные_функции
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
